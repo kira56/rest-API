cceab2befe96c4e79b66b138f600e914c7e0e6bf
kira56 <ridhunter1508@gmail.com>
2020-05-11 16:11:33 -0500

    commit para heroku

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,26 @@
 // Puerto
 // ===============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===============================
+// Enviroment - Entorno
+// ===============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ===============================
+// Base de Datos
+// ===============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafeDB';
+} else {
+    urlDB = 'mongodb+srv://kira56:Tomcat1508@cluster0-vtpre.mongodb.net/cafeDB'
+}
+process.env.URLDB = urlDB;
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config')
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 const app = express()
 
@@ -9,30 +10,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function (req, res) {
-    res.json('GET de usuarios')
-})
+// controladores
+app.use(require('./controllers/usuario'))
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
-
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok:false,
-            message:'El nombre es necesario',
-        });
-    }else{
-        res.json({persona:body})
-    }
-})
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({id})
-})
-app.delete('/usuario', function (req, res) {
-    res.json('DELETE de usuarios')
-})
-
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}, (err) => {
+    if (err) throw err;
+    console.log('Base de datos online')
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto', 3000)
